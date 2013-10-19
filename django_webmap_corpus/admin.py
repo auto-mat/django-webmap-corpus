937ee36a7e8c89a075cf7963e498e4643ae736b6
Petr Dlouhý <petr.dlouhy@email.cz>
2013-10-19 11:37:11 +0200

    model and admin imported from Zelena mapa

--- django_webmap_corpus/admin.py
+++ django_webmap_corpus/admin.py
@@ -1,11 +1,234 @@
-"""Admin classes for the django_webmap_corpus app."""
-# from django.contrib import admin
+# -*- coding: utf-8 -*-
+# admin.py
 
-# from . import models
+from django.conf import settings # needed if we use the GOOGLE_MAPS_API_KEY from settings
+
+# Import the admin site reference from django.contrib.admin
+from django.contrib import admin
+from django.contrib.admin import SimpleListFilter
+#from import_export.admin import ImportExportModelAdmin
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.admin import UserAdmin
+from django.db.models import Q
+from constance import config
+import fgp
+from django.core.urlresolvers import reverse
+
+# Grab the Admin Manager that automaticall initializes an OpenLayers map
+# for any geometry field using the in Google Mercator projection with OpenStreetMap basedata
+from django.contrib.gis.admin import OSMGeoAdmin
+from django.contrib.gis.geos import Point
+
+# Note, another simplier manager that does not reproject the data on OpenStreetMap is available
+# with from `django.contrib.gis.admin import GeoModelAdmin`
+
+# Finally, import our model from the working project
+# the geographic_admin folder must be on your python path
+# for this import to work correctly
+from models import *
+
+USE_GOOGLE_TERRAIN_TILES = False
+
+class UserAdmin(UserAdmin):
+    list_display = ('__unicode__', 'email', 'first_name', 'last_name', 'is_staff', 'is_superuser', 'is_active', 'last_login', 'get_groups', 'get_user_permissions')
+
+    def get_groups(self, obj):
+        if obj:
+            return ", ".join([group.name for group in obj.groups.all()])
+
+    def get_user_permissions(self, obj):
+        if obj:
+            return ", ".join([user_permission.name for user_permission in obj.user_permissions.all()])
 
 
-# class YourModelAdmin(admin.ModelAdmin):
-#    list_display = ['some', 'fields', ]
-#    search_fields = ['some', 'fieds', ]
+class SektorFilter(SimpleListFilter):
+    title = (u"Sektor")
+    parameter_name = u"sektor"
 
-# admin.site.register(models.YourModel, YourModelAdmin)
+    def lookups(self, request, model_admin):
+        return [("mimo", u"Mimo sektory")] + [(sektor.slug, sektor.nazev) for sektor in Sektor.objects.all()]
+
+    def queryset(self, request, queryset):
+        if not self.value():
+            return queryset
+        if self.value() == "mimo":
+            for sektor in Sektor.objects.all():
+                queryset = queryset.exclude(geom__contained = sektor.geom)
+            return queryset
+        return queryset.filter(geom__contained = Sektor.objects.get(slug = self.value()).geom)
+
+class PoiStatusFilter(SimpleListFilter):
+    title = (u"Všechny statuty")
+    parameter_name = u"statuty"
+
+    def lookups(self, request, model_admin):
+        return ((None, u"Viditelné"),
+                ('all', _('All')),
+                ("skryte", u"Skryté"))
+
+    def choices(self, cl):
+        for lookup, title in self.lookup_choices:
+            yield {
+                'selected': self.value() == lookup,
+                'query_string': cl.get_query_string({
+                    self.parameter_name: lookup,
+                }, []),
+                'display': title,
+            }
+
+    def queryset(self, request, queryset):
+        if self.value() == 'all':
+            return queryset
+        if not self.value() or self.value() == "viditelne":
+            return queryset.filter(Q(status__show_TU = True) & Q(znacka__status__show_TU = True) & Q(znacka__vrstva__status__show_TU = True))
+        if self.value() == "skryte":
+            return queryset.exclude(Q(status__show_TU = True) & Q(znacka__status__show_TU = True) & Q(znacka__vrstva__status__show_TU = True))
+
+@fgp.enforce
+class PoiAdmin(OSMGeoAdmin):#, ImportExportModelAdmin):
+    model = Poi
+    list_display = ['nazev','status','znacka','url','foto_thumb', 'desc', 'id' ]
+    list_filter = (PoiStatusFilter, 'status', SektorFilter, 'znacka__vrstva', 'znacka',)
+    exclude = ('vlastnosti_cache', )
+    readonly_fields = ("created_at", "author")
+    raw_id_fields = ('znacka',)
+    search_fields = ('nazev',)
+    ordering = ('nazev',)
+    save_as = True
+    search_fields = ['nazev']
+    list_select_related = True
+    filter_horizontal = ('vlastnosti',)
+    list_max_show_all = 10000
+
+    if USE_GOOGLE_TERRAIN_TILES:
+      map_template = 'gis/admin/google.html'
+      extra_js = ['http://openstreetmap.org/openlayers/OpenStreetMap.js', 'http://maps.google.com/maps?file=api&amp;v=2&amp;key=%s' % settings.GOOGLE_MAPS_API_KEY]
+    else:
+      pass # defaults to OSMGeoAdmin presets of OpenStreetMap tiles
+
+    # Default GeoDjango OpenLayers map options
+    # Uncomment and modify as desired
+    # To learn more about this jargon visit:
+    # www.openlayers.org
+    
+    def get_form(self, request, obj=None, **kwargs):
+         pnt = Point(config.MAP_BASELON, config.MAP_BASELAT, srid=4326)
+         pnt.transform(900913)
+         self.default_lon, self.default_lat = pnt.coords
+
+         if not request.user.is_superuser and request.user.has_perm(u'mapa.can_only_own_data_only') and obj and obj.author != request.user:
+             self.fields = ('nazev', )
+             self.readonly_fields = ('nazev', )
+         else:
+             self.fields = PoiAdmin.fields
+             self.readonly_fields = PoiAdmin.readonly_fields
+         return super(PoiAdmin, self).get_form(request, obj, **kwargs)
+
+    default_zoom = 12
+    #display_wkt = False
+    #display_srid = False
+    #extra_js = []
+    #num_zoom = 18
+    #max_zoom = False
+    #min_zoom = False
+    #units = False
+    #max_resolution = False
+    #max_extent = False
+    #modifiable = True
+    #mouse_position = True
+    #scale_text = True
+    #layerswitcher = True
+    scrollable = False
+    #admin_media_prefix = settings.ADMIN_MEDIA_PREFIX
+    map_width = 700
+    map_height = 500
+    map_srid = 900913
+    #map_template = 'gis/admin/openlayers.html'
+    #openlayers_url = 'http://openlayers.org/api/2.6/OpenLayers.js'
+    #wms_url = 'http://labs.metacarta.com/wms/vmap0'
+    #wms_layer = 'basic'
+    #wms_name = 'OpenLayers WMS'
+    #debug = False
+    #widget = OpenLayersWidget
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.author = request.user # no need to check for it.
+        obj.save()
+
+class SektorAdmin(OSMGeoAdmin):
+    list_display = ('nazev',)
+    prepopulated_fields = {'slug': ('nazev',) } # slug se automaticky vytvari z nazvu
+    if USE_GOOGLE_TERRAIN_TILES:
+      map_template = 'gis/admin/google.html'
+      extra_js = ['http://openstreetmap.org/openlayers/OpenStreetMap.js', 'http://maps.google.com/maps?file=api&amp;v=2&amp;key=%s' % settings.GOOGLE_MAPS_API_KEY]
+    else:
+      pass # defaults to OSMGeoAdmin presets of OpenStreetMap tiles
+
+    def get_form(self, request, obj=None, **kwargs):
+         pnt = Point(config.MAP_BASELON, config.MAP_BASELAT, srid=4326)
+         pnt.transform(900913)
+         self.default_lon, self.default_lat = pnt.coords
+         return super(SektorAdmin, self).get_form(request, obj, **kwargs)
+
+    default_zoom = 12
+    scrollable = False
+    map_width = 700
+    map_height = 500
+    map_srid = 900913
+
+class ZnackaInline(admin.TabularInline):
+    model = Znacka
+
+class VrstvaAdmin(admin.ModelAdmin): #ImportExportModelAdmin):
+    prepopulated_fields = {'slug': ('nazev',) } # slug se automaticky vytvari z nazvu
+    list_display = ['nazev', 'status', 'order']
+    inlines = [ZnackaInline]
+
+class MapaAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('nazev',) } # slug se automaticky vytvari z nazvu
+
+class VlastnostAdmin(admin.ModelAdmin):
+    list_display = ('nazev', 'filtr', 'status')
+    prepopulated_fields = {'slug': ('nazev',) } # slug se automaticky vytvari z nazvu
+    model = Vlastnost
+
+class ZnackaAdmin(admin.ModelAdmin):
+    list_display = ('nazev', 'desc', 'vrstva', 'minzoom', 'status', 'default_icon_image', 'id', 'poi_count')
+    list_filter = ('vrstva','status',)
+    search_fields = ('nazev', 'desc',)
+    readonly_fields = ('poi_count',)
+
+    def default_icon_image(self, obj):
+        if obj.default_icon:
+            return '<img src="%s"/>' % obj.default_icon.url
+    default_icon_image.short_description = "icon"
+    default_icon_image.allow_tags = True
+
+    def get_form(self, request, obj=None, **kwargs):
+        if not request.user.is_superuser and request.user.has_perm(u'mapa.can_only_view'):
+            self.fields = ('nazev', )
+            self.readonly_fields = ('nazev', )
+        else:
+            self.fields = ZnackaAdmin.fields
+            self.readonly_fields = ZnackaAdmin.readonly_fields
+        return super(ZnackaAdmin, self).get_form(request, obj, **kwargs)
+
+    def poi_count(self, obj):
+        url = reverse('admin:django_webmap_corpus_poi_changelist')
+        return '<a href="{0}?znacka__id__exact={1}&amp;statuty=all">{2}</a>'.format(url, obj.id, obj.pois.count())
+    poi_count.short_description = "Count"
+    poi_count.allow_tags = True
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('nazev', 'desc', 'show', 'show_TU')
+    
+admin.site.register(Poi   , PoiAdmin   )
+admin.site.register(Vrstva, VrstvaAdmin)
+admin.site.register(Sektor, SektorAdmin)
+admin.site.register(Znacka, ZnackaAdmin)
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Vlastnost, VlastnostAdmin)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- django_webmap_corpus/models.py
+++ django_webmap_corpus/models.py
@@ -1,3 +1,192 @@
-"""Just an empty models file to let the testrunner recognize this as app."""
-# from django.db import models
-# from django.utils.translation import ugettext_lazy as _
+# -*- coding: utf-8 -*-
+import datetime
+import fgp
+
+from django.contrib.gis.db import models
+from django.utils.safestring import mark_safe
+from django.core.cache import cache
+
+from django.contrib.auth.models import User
+from colorful.fields import RGBColorField
+
+from .utils import SlugifyFileSystemStorage
+
+class Status(models.Model):
+    "Stavy zobrazeni konkretniho objektu, vrstvy apod. - aktivni, navrzeny, zruseny, ..."
+    nazev   = models.CharField(unique=True, max_length=255, help_text=u"Název statutu")
+    desc    = models.TextField(null=True, blank=True, help_text=u"Popis")
+    show    = models.BooleanField(help_text=u"Zobrazit uživateli zvenčí")
+    show_TU = models.BooleanField(help_text=u"Zobrazit editorovi mapy")
+
+    class Meta:
+        verbose_name_plural = "statuty"
+    def __unicode__(self):
+        return self.nazev
+
+class Vrstva(models.Model):
+    "Vrstvy, ktere se zobrazi v konkretni mape"
+    nazev   = models.CharField(max_length=255)                      # Name of the layer
+    slug    = models.SlugField(unique=True, verbose_name=u"název v URL")  # Vrstva v URL
+    desc    = models.TextField(null=True, blank=True)               # Description
+    status  = models.ForeignKey(Status)              # zobrazovaci status
+    order   = models.PositiveIntegerField()
+    remark  = models.TextField(null=True, blank=True, help_text=u"interni informace o objektu, ktere se nebudou zobrazovat")
+
+    class Meta:
+        verbose_name_plural = u"vrstvy"
+        ordering = ['order']
+    def __unicode__(self):
+        return self.nazev
+
+class Znacka(models.Model):
+    "Mapove znacky vcetne definice zobrazeni"
+    nazev   = models.CharField(unique=True, max_length=255)   # Name of the mark
+    
+    # Relationships
+    vrstva  = models.ForeignKey(Vrstva)              # Kazda znacka lezi prave v jedne vrstve
+    status  = models.ForeignKey(Status)              # kvuli vypinani
+    
+    # icon: Neni zde, ale v tabulce znacky a vztahuje se k rozlicnym zobrazenim 
+    # Pro zjednoduseni mame image "default_icon", ale to je jen nouzove reseni, 
+    # ktere nesmi nahradit system znacek zavislych na zobrazeni, etc.
+    
+    # content 
+    desc    = models.TextField(null=True, blank=True, help_text=u"podrobny popis znacky")
+    remark  = models.TextField(null=True, blank=True, help_text=u"interni informace o objektu, ktere se nebudou zobrazovat")
+    
+    # Base icon and zoom dependent display range
+    default_icon = models.ImageField(null=True, blank=True, upload_to='ikony', storage=SlugifyFileSystemStorage())
+    minzoom = models.PositiveIntegerField(default=1)
+    maxzoom = models.PositiveIntegerField(default=10)
+
+    # Linear elements style
+    line_width = models.FloatField( verbose_name=u"šířka čáry", default=2,)
+    line_color = RGBColorField(default="#ffc90e")
+    def line_color_kml(this):
+        color = this.line_color[1:]
+        return "88" + color[4:6] + color[2:4] + color[0:2]
+
+    url     = models.URLField(null=True, blank=True, help_text=u"ukáže se u všech míst s touto značkou, pokud nemají vlastní url")
+    
+    class Meta:
+        permissions = [
+            ("can_only_view", "Can only view"),
+        ]
+        verbose_name_plural = "značky"
+        ordering = ['-vrstva__order', 'nazev']
+
+    def __unicode__(self):
+        return self.nazev
+
+class ViditelneManager(models.GeoManager):
+    "Pomocny manazer pro dotazy na Poi se zobrazitelnym statuem"
+    def get_query_set(self):
+        return super(ViditelneManager, self).get_query_set().filter(status__show=True, znacka__status__show=True)
+
+class Sektor(models.Model):
+    "Sektor mapy"
+    nazev   = models.CharField(max_length=255)
+    slug    = models.SlugField(unique=True, verbose_name="Slug")
+    
+    geom    = models.PolygonField(verbose_name=u"plocha",srid=4326, help_text=u"Plocha sektoru")
+    stred   = models.PointField(verbose_name=u"Poloha středu sektoru",srid=4326, null=True)
+    objects = models.GeoManager()
+
+    class Meta:
+        verbose_name_plural = u"sektory"
+
+@fgp.guard('dulezitost', 'status', name='can_edit_advanced_fields')
+class Poi(models.Model):
+    "Misto - bod v mape"
+    author = models.ForeignKey(User, verbose_name="Autor")
+
+    nazev   = models.CharField(max_length=255, verbose_name=u"název", help_text=u"Přesný název místa.")
+    
+    # Relationships
+    znacka  = models.ForeignKey(Znacka, limit_choices_to = {'status__show_TU': 'True', 'vrstva__status__show_TU': 'True'}, verbose_name=u"značka", help_text="Zde vyberte ikonu, která se zobrazí na mapě.", related_name="pois")
+    status  = models.ForeignKey(Status, default=2, help_text="Status místa; určuje, kde všude se místo zobrazí.")
+    vlastnosti    = models.ManyToManyField('Vlastnost', blank=True, null=True, help_text="Určete, jaké má místo vlastnosti. Postupujte podle manuálu.<br/>")
+    
+    # "dulezitost" - modifikator minimalniho zoomu, ve kterem se misto zobrazuje. 
+    dulezitost = models.SmallIntegerField(default=0, verbose_name=u"důležitost",
+                 help_text=u"""Modifikátor minimalniho zoomu, ve kterém se místo zobrazuje (20+ bude vidět vždy).<br/>
+                               Cíl je mít výběr základních objektů viditelných ve velkých měřítcích
+                               a zabránit přetížení mapy značkami v přehledce.<br/>
+                               Lze použít pro placenou reklamu! ("Váš podnik bude vidět hned po otevření mapy")""")
+    
+    # Geographical intepretation
+    geom    = models.GeometryField(verbose_name=u"poloha",srid=4326, help_text=u"""Vložení bodu: Klikněte na tužku s plusem a umístěte bod na mapu<br/>
+            Kreslení linie: Klikněte na ikonu linie a klikáním do mapy určete lomenou čáru. Kreslení ukončíte dvouklikem.<br/>
+            Kreslení oblasti: Klikněte na ikonu oblasti a klikáním do mapy definujte oblast. Kreslení ukončíte dvouklikem.<br/>
+            Úprava vložených objektů: Klikněte na první ikonu a potom klikněte na objekt v mapě. Tažením přesouváte body, body uprostřed úseků slouží k vkládání nových bodů do úseku.""")
+    objects = models.GeoManager()
+    
+    # Own content (facultative)
+
+    desc    = models.TextField(null=True, blank=True, verbose_name=u"popis", help_text=u"Text, který se zobrazí na mapě po kliknutí na ikonu.")
+    desc_extra = models.TextField(null=True, blank=True, verbose_name=u"podrobný popis", help_text="Text, který rozšiřuje informace výše.")
+    url     = models.URLField(null=True, blank=True, help_text=u"Odkaz na webovou stránku místa.")
+    remark  = models.TextField(null=True, blank=True, verbose_name=u"interní poznámka", help_text=u"Interní informace o objektu, které se nebudou zobrazovat.")
+
+    # navzdory nazvu jde o fotku v plnem rozliseni
+    foto_thumb  = models.ImageField(null=True, blank=True,
+                                    upload_to='foto', storage=SlugifyFileSystemStorage(),
+                                    verbose_name=u"fotka",
+                                    help_text=u"Nahrajte fotku v plné velikosti.",
+                                   )
+    # zde se ulozi slugy vsech vlastnosti, aby se pri renederovani kml
+    # nemusel delat db dotaz pro kazde Poi na jeho vlastnosti
+    vlastnosti_cache = models.CharField(max_length=255, null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True,  verbose_name="Posledni zmena")
+    
+    viditelne = ViditelneManager()
+    
+    class Meta:
+        permissions = [
+            ("can_only_own_data_only", "Can only edit his own data"),
+        ]
+        verbose_name = "místo"
+        verbose_name_plural = "místa"
+    def __unicode__(self):
+        return self.nazev
+    def save_vlastnosti_cache(self):
+        self.vlastnosti_cache = u",".join([v.slug for v in self.vlastnosti.filter(status__show=True)])
+        self.save()
+    def get_absolute_url(self):
+        return "/misto/%i/" % self.id
+
+    def save(self, *args, **kwargs):
+        self.created_at = datetime.datetime.now()
+        super(Poi, self).save(*args, **kwargs)
+
+from django.db.models.signals import m2m_changed, post_save, post_delete
+def update_vlastnosti_cache(sender, instance, action, reverse, model, pk_set, **kwargs):
+    "Aktualizace cache vlastnosti pri ulozeni Poi. Je treba jeste vyresit smazani Vlastnosti"
+    if action == 'post_add':
+        instance.save_vlastnosti_cache()
+m2m_changed.connect(update_vlastnosti_cache, Poi.vlastnosti.through) 
+
+def invalidate_cache(sender, instance, **kwargs):
+    if sender in [Status, Vrstva, Znacka, Poi, Vlastnost]:
+        cache.clear()
+post_save.connect(invalidate_cache)
+post_delete.connect(invalidate_cache)
+    
+class Vlastnost(models.Model):
+    "Vlastnosti mist"
+    nazev   = models.CharField(max_length=255)   # Name of the property
+    status  = models.ForeignKey(Status)          # "Statuty"  - tj. active/inactive. Mozny je i boolean "active"
+    filtr   = models.BooleanField()              # Pouzit v levem menu, filtrovat???
+    poradi  = models.PositiveIntegerField()
+    # content 
+    slug    = models.SlugField(unique=True, verbose_name="Slug")  # Popis tagu v URL
+    desc    = models.TextField(null=True, blank=True) # podrobny popis vlastnosti
+    remark  = models.TextField(null=True, blank=True, help_text=u"interni informace o objektu, ktere se nebudou zobrazovat")
+    default_icon = models.ImageField(null=True, blank=True, upload_to='ikony', storage=SlugifyFileSystemStorage())
+   
+    class Meta:
+        verbose_name_plural = u"vlastnosti"
+	ordering = ['poradi']
+    def __unicode__(self):
+        return self.nazev
