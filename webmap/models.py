bda55d808a53b83cdc0ca35fe6ec1f0759a7d825
Petr Dlouhý <petr.dlouhy@email.cz>
2014-01-07 14:35:44 +0100

    imges order depends on POI

--- webmap/models.py
+++ webmap/models.py
@@ -269,7 +269,7 @@ class Photo(models.Model):
     name = models.CharField(max_length=255, verbose_name=_(u"name"), help_text=_(u"Photo name"))
     desc = models.TextField(null=True, blank=True, verbose_name=_("description"), help_text=_(u"Photo description."))
     license = models.ForeignKey(License, verbose_name=_("license"))
-    order = PositionField(verbose_name=_("order"))
+    order = PositionField(verbose_name=_("order"), collection="poi")
     photographer = models.CharField(max_length=255, verbose_name=_(u"Photography author"), blank=True, help_text=_(u"Full name of the author of the photography"))
 
     photo = models.ImageField(null=False, blank=False,
