099513d95a9b0cb22454fc546441e6f2d8cff11e
Petr Dlouhý <petr.dlouhy@email.cz>
2014-01-28 14:55:19 +0100

    search_view to webmap corpus

--- webmap/urls.py
+++ webmap/urls.py
@@ -1,9 +1,10 @@
 """URLs for the webmap app."""
 from django.conf.urls import patterns
-from webmap import views
+from webmap import views, search_view
 
 
 urlpatterns = patterns(
     '',
     (r'^kml/([-\w]+)/$', views.kml_view),
+    (r'^search/([- \w]+)/$', search_view),
 )
--- webmap/views.py
+++ webmap/views.py
@@ -2,10 +2,12 @@
 # from django.views.generic import TemplateView
 
 from webmap import models
+from django import http
 from django.views.decorators.gzip import gzip_page
 from django.views.decorators.cache import never_cache, cache_page
 from django.shortcuts import get_object_or_404
 from django.contrib.gis.shortcuts import render_to_kml
+from django.db.models import Q
 
 
 @gzip_page
@@ -18,3 +20,21 @@ def kml_view(request, layer_name):
     # all enabled pois in this layer
     points = models.Poi.visible.filter(marker__layer=v).kml()
     return render_to_kml("webmap/gis/kml/layer.kml", {'places': points})
+
+
+def search_view(request, query):
+    if len(query) < 3:
+        return http.HttpResponseBadRequest('Insufficient query lenght')
+
+    # first by name
+    name_qs = models.Poi.visible.filter(Q(name__icontains=query))
+    # then by description, address and marker name if not done before
+    extra_qs = models.Poi.visible.filter(
+        Q(desc__icontains=query)
+        | Q(address__icontains=query)
+        | Q(marker__name__icontains=query)).exclude(id__in=name_qs)
+    # union qs doesn't hold order, so transform to lists and join
+    points = list(name_qs.kml()) + list(extra_qs.kml())
+    return render_to_kml("webmap/gis/kml/layer.kml", {
+                         'places': points,
+                         })
