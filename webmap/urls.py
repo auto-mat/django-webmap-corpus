c648cf01c1917346a39f7b9c8d8b80b54af2cbaa
Petr Dlouhý <petr.dlouhy@email.cz>
2014-01-28 14:58:41 +0100

    fix search_view urls import

--- webmap/urls.py
+++ webmap/urls.py
@@ -1,10 +1,10 @@
 """URLs for the webmap app."""
 from django.conf.urls import patterns
-from webmap import views, search_view
+from webmap import views
 
 
 urlpatterns = patterns(
     '',
     (r'^kml/([-\w]+)/$', views.kml_view),
-    (r'^search/([- \w]+)/$', search_view),
+    (r'^search/([- \w]+)/$', views.search_view),
 )
