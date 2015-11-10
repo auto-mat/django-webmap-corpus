5ca16f72fe8987a026ce0a9b0782ca64079808c4
Petr Dlouhý <petr.dlouhy@email.cz>
2015-11-10 16:51:20 +0100

    add more tests

--- webmap/tests/models_tests.py
+++ webmap/tests/models_tests.py
@@ -10,7 +10,7 @@ class WebmapTests(TestCase):
     def setUp(self):
                 # Every test needs access to the request factory.
         self.factory = RequestFactory()
-        self.user = User.objects.create_user(
+        self.user = User.objects.create_superuser(
             username='test_user', email='test_user@test_user.com', password='top_secret')
         self.user.save()
 
@@ -21,4 +21,23 @@ class WebmapTests(TestCase):
         from webmap import views
         self.assertTrue(self.client.login(username='test_user', password='top_secret'))
         response = self.client.get(reverse(views.kml_view, args=("l",)))
+        self.assertEqual(response.status_code, 200)
+
+    def test_search_view(self):
+        """
+        test if search view
+        """
+        from webmap import views
+        self.assertTrue(self.client.login(username='test_user', password='top_secret'))
+        response = self.client.get(reverse(views.search_view, args=("asdf",)))
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_views(self):
+        """
+        test if we can see all admin pages
+        """
+        self.assertTrue(self.client.login(username='test_user', password='top_secret'))
+        response = self.client.get(reverse("admin:webmap_marker_changelist"))
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse("admin:webmap_overlaylayer_changelist"))
         self.assertEqual(response.status_code, 200)
